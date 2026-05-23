9641deadbbdef5ad9e6950889a26cd04d5a577ee
Ricardo Albrecht <ricardoalbrecht1@gmail.com>
2026-05-23 20:32:36 -0300

    improving something

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.API_URL || 'http://localhost:3000/api';
 
 export const api = axios.create({
     baseURL,
-    withCredentials: true, 
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
